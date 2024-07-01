bff8a3bf362560f6fa1961837957a0544a4b91c2
Junaid khan <jk387520@gmail.com>
2024-07-01 19:18:29 +0530

    Changing files

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,16 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 5176,    
-    proxy: {
-      '/api': {
-        target: `https://video-sharing-app-backend-fcv2.onrender.com/` ,
-        changeOrigin: true,
-        secure: false,
-        ws: false
-      }
-    }
-  },
+  // server: {
+  //   port: 5176,    
+  //   proxy: {
+  //     '/api': {
+  //       target: `https://video-sharing-app-backend-fcv2.onrender.com/` ,
+  //       changeOrigin: true,
+  //       secure: false,
+  //       ws: false
+  //     }
+  //   }
+  // },
   plugins: [react()],
 })
