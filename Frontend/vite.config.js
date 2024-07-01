5ef9ef9a015922e33560816523f1c1eb383bf752
Junaid khan <jk387520@gmail.com>
2024-07-01 15:28:40 +0530

    Changing conf

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // port: 5176,    
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:5000',
-    //     changeOrigin: true,
-    //     secure: false,
-    //     ws: false
-    //   }
-    // }
+    port: 5176,    
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        ws: false
+      }
+    }
   },
   plugins: [react()],
 })
